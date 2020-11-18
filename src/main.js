3c0df7391882b285398aaea2584df980135c17a7
Borja García Villanueva <borjagarciavillanueva@gmail.com>
2020-11-18 12:31:22 +0100

    Revert "Feature/user form"

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-const myApp = createApp(App);
-myApp.use(router);
-myApp.mount('#app');
+createApp(App).use(router).mount('#app');
