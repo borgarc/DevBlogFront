a72af2448a81441f219c3c4278cfd212420854d2
borgarc <borjagarciavillanueva@gmail.com>
2020-11-20 15:57:17 +0100

    Created navbar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,74 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <anchor @click="goToHome()" class="navbar-brand" href="#">RogueLikeGames</anchor>
+    <button class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav rl-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Create Post <span class="sr-only">(current)</span></a>
+        </li>
+      </ul>
+      <ul v-if="userNick==null" class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <anchor @click="goToLogIn()"
+            class="nav-link"
+            href="#">Log In
+            <span class="sr-only">(current)</span></anchor>
+        </li>
+        <li class="nav-item active">
+          <anchor @click="goToCreateUser()"
+            class="nav-link"
+            href="#">Register
+            <span class="sr-only">(current)</span></anchor>
+        </li>
+      </ul>
+      <ul v-if="userNick!=null" class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <anchor @click="goToLogIn()"
+            class="nav-link"
+            href="#">{{ userNick }}
+            <span class="sr-only">(current)</span></anchor>
+        </li>
+        <li class="nav-item active">
+          <anchor @click="logOut()"
+            class="nav-link"
+            href="#">Log Out
+            <span class="sr-only">(current)</span></anchor>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    userNick: String,
+  },
+  methods: {
+    logOut() {
+      // localStorage.removeItem('username');
+      // localStorage.removeItem('token');
+      this.$emit('log-out', null);
+      // this.username = null;
+    },
+    goToCreateUser() {
+      this.$router.push({ name: 'User' });
+    },
+    goToLogIn() {
+      this.$router.push({ name: 'LogIn' });
+    },
+    goToHome() {
+      this.$router.push({ name: 'Home' });
+    },
+  },
+};
+</script>
